1653cad4d5380a94ebdee608f4ad8668c7125d8a
Mohamed Elafifi <maelafifi@users.noreply.github.com>
2018-02-23 12:01:10 -0800

    Create pdfDesigner.java

---
+++ algorithms/implementations/pdfDesigner.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int designerPdfViewer(int[] h, String word) {
+        int max = 0;
+        for(int i = 0; i < word.length(); i++){
+            if(h[(int)word.charAt(i)-97] > max){
+                max = h[(int)word.charAt(i)-97];
+            }
+        }
+        return max*word.length();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] h = new int[26];
+        for(int h_i = 0; h_i < 26; h_i++){
+            h[h_i] = in.nextInt();
+        }
+        String word = in.next();
+        int result = designerPdfViewer(h, word);
+        System.out.println(result);
+        in.close();
+    }
+}
